bd92cc22beeb7dd8db185c38e32a4da8f148a34d
Manik Dahiya <dahiyamanik@gmail.com>
2021-04-27 21:20:49 +0530

    First Commit

---
+++ TipCalculator/TipCalculator/TipCalculator.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace TipCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDownTip = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownPeople = new System.Windows.Forms.NumericUpDown();
+            this.labelTip = new System.Windows.Forms.Label();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.textBoxBill = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTip)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPeople)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(104, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bill";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(104, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tip%";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 301);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of People";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(402, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tip";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(404, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "per person";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(402, 227);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Total";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(404, 252);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(77, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "per person";
+            // 
+            // numericUpDownTip
+            // 
+            this.numericUpDownTip.Location = new System.Drawing.Point(108, 199);
+            this.numericUpDownTip.Maximum = new decimal(new int[] {
+            101,
+            0,
+            0,
+            0});
+            this.numericUpDownTip.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDownTip.Name = "numericUpDownTip";
+            this.numericUpDownTip.ReadOnly = true;
+            this.numericUpDownTip.Size = new System.Drawing.Size(131, 22);
+            this.numericUpDownTip.TabIndex = 9;
+            this.numericUpDownTip.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericUpDownPeople
+            // 
+            this.numericUpDownPeople.Location = new System.Drawing.Point(108, 324);
+            this.numericUpDownPeople.Maximum = new decimal(new int[] {
+            101,
+            0,
+            0,
+            0});
+            this.numericUpDownPeople.Name = "numericUpDownPeople";
+            this.numericUpDownPeople.ReadOnly = true;
+            this.numericUpDownPeople.Size = new System.Drawing.Size(131, 22);
+            this.numericUpDownPeople.TabIndex = 12;
+            this.numericUpDownPeople.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDownPeople.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // labelTip
+            // 
+            this.labelTip.AutoSize = true;
+            this.labelTip.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTip.Location = new System.Drawing.Point(566, 118);
+            this.labelTip.Name = "labelTip";
+            this.labelTip.Size = new System.Drawing.Size(0, 25);
+            this.labelTip.TabIndex = 13;
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotal.Location = new System.Drawing.Point(566, 235);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(0, 25);
+            this.labelTotal.TabIndex = 14;
+            // 
+            // textBoxBill
+            // 
+            this.textBoxBill.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.textBoxBill.Location = new System.Drawing.Point(108, 93);
+            this.textBoxBill.Name = "textBoxBill";
+            this.textBoxBill.Size = new System.Drawing.Size(131, 22);
+            this.textBoxBill.TabIndex = 8;
+            this.textBoxBill.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxBill.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxBill_KeyPress);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(724, 427);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 15;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.textBoxBill);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.labelTip);
+            this.Controls.Add(this.numericUpDownPeople);
+            this.Controls.Add(this.numericUpDownTip);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Tip Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownTip)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPeople)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDownTip;
+        private System.Windows.Forms.NumericUpDown numericUpDownPeople;
+        private System.Windows.Forms.Label labelTip;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.TextBox textBoxBill;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ TipCalculator/TipCalculator/TipCalculator.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+      Assignment - Tip Calculator
+      Date       - 27-04-2021
+
+                   Calculates the total amount of bill and tip to be paid per person
+ */ 
+namespace TipCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxBill.TextChanged += TextBoxBill_TextChanged;                      //occurs when Bill is entered
+            numericUpDownTip.ValueChanged += NumericUpDownTip_ValueChanged;          //occurs when Tip is entered
+            numericUpDownPeople.ValueChanged += NumericUpDownPeople_ValueChanged;    //occurs when Number of People is entered
+
+        }
+        
+        private void Calculate()
+        {
+
+            // calculates and shows the amount of bill and tip to be paid by each person entered by the user
+            
+            double Bill;                                                   //amount of Bill
+            int Tip;                                                       //amount of Tip
+            int noOfPeople;                                                //number of people
+            double tipPerPerson;                                           //amount of tipPerPerson
+            double totalPerPerson;                                         //amount of total bill per person
+
+            double.TryParse(textBoxBill.Text, out Bill);                   //assigning Bill variable to textBoxBill 
+            Tip = Convert.ToInt32(numericUpDownTip.Value);                 //assigning Tip to numericUpDownTip
+            noOfPeople = Convert.ToInt32(numericUpDownPeople.Value);       //assigning noOfPeople to numericUpDownPeople
+            tipPerPerson = (Bill * Tip) / (noOfPeople * 100);              //calculating Tip to be paid by each person
+            labelTip.Text = tipPerPerson.ToString("c");                    //showing Tip to be paid by each person
+            totalPerPerson = (Bill / noOfPeople) + tipPerPerson;           //calculating Amount to be paid by each person
+            labelTotal.Text = totalPerPerson.ToString("c");                //showing Amount to be paid by each person
+        }
+        
+        private void TextBoxBill_TextChanged(object sender, EventArgs e)
+        {
+            //function for Bill box
+            
+            Calculate();                                                    //calculate function is called
+        
+        }
+
+        private void NumericUpDownTip_ValueChanged(object sender, EventArgs e)
+        {
+            //function for Tip box
+
+            if (numericUpDownTip.Value < 0)                                 //statement to check whether user enters negative tip  
+            {
+                numericUpDownTip.Value = 0;
+                MessageBox.Show("Tip cannot be negative!");
+            }
+            if (numericUpDownTip.Value > 100)                               //statement to check whether user enters tip out of maximum limit
+            {
+                numericUpDownTip.Value = 0;              
+                MessageBox.Show("Maximum Tip reached!");
+            }
+            Calculate();
+
+        }
+
+        private void NumericUpDownPeople_ValueChanged(object sender, EventArgs e)
+        {
+            //function for Number of People Box
+            
+            if (numericUpDownPeople.Value == 0)                             //statement to check whether user enters 0 number of people      
+            {
+                numericUpDownPeople.Value = 1;
+                MessageBox.Show("Number of People cannot be 0!");
+            }
+            if (numericUpDownPeople.Value > 100)                            //statement to check whether user enters number of people out of maximum limit
+            {
+                numericUpDownPeople.Value = 1;
+                MessageBox.Show("Maximum Occupancy Reached!");
+            }
+             Calculate();
+        }
+        private void textBoxBill_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //function to accept only decimal whole numbers
+
+            if (e.Handled = !char.IsDigit(e.KeyChar) && e.KeyChar != 8 &&e.KeyChar!=46)       //statement to check whether user enters alpahbets or negative numbers in textBoxBill
+            {
+                MessageBox.Show("Accepts only numeric values");
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            //function to clear the labels and boxes
+            
+            textBoxBill.Clear();                                            
+            numericUpDownTip.Value = 0;
+            numericUpDownPeople.Value = 1;
+            labelTip.Text.Remove(0);
+            labelTotal.Text.Remove(0);
+        }
+    }
+}
